bbe135b16276833769cad6b1e033578308bbc98d
krista-school <130492972+krista-school@users.noreply.github.com>
2023-04-13 10:51:48 +0300

    Update src.js

--- src.js
+++ src.js
@@ -46,10 +46,10 @@ $('body').terminal({
             this.echo("PELIJUTUT:\npeli_strategia_ohjeet.exe\nWorldOfWarcraft\nDiscord\n")
         }
         if (systemOnlineMgmt == true && hackcheck == false){
-            this.echo("HOME:\nKorjausohje\nEmail\n\nSYSTEM CONTROL TOOLS:\nHackCheckPro\nSecuritySetUp\nFileChecker\n")
+            this.echo("HOME:\nKorjausohje\nEmail\n\nSYSTEM CONTROL TOOLS:\nHackCheckPro\n\nFileChecker\n")
         }
         if (hackcheck == true && fix == false){
-            this.echo("HOME:\nKorjausohje\nEmail\n(Uusi) lolh4x\n\nSYSTEM CONTROL TOOLS:\nHackCheckPro\nSecuritySetup\nFileChecker\n")
+            this.echo("HOME:\nKorjausohje\nEmail\n(Uusi) lolh4x\n\nSYSTEM CONTROL TOOLS:\nHackCheckPro\nFileChecker\n")
         }
         if (hackcheck == true && fix == true){
             this.echo("HOME:\nKorjausohje\nEmail\nlolh4x\n\nSYSTEM CONTROL TOOLS:\nHackCheckPro\nFileChecker\n")
